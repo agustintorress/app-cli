366488a6e62057cc2cb1b54775f324ced10fcc37
agustintorress <agustintorresberbere@gmail.com>
2022-08-24 07:31:38 -0300

    holi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import personas from "./data/personas.json";
+
 const App = () => {
   return (
-    <div className="App">
-      <p>Lorem ipsum dolor sit amet.</p>
-      {
-        personas.map(persona => (
-          <div>
-            <p>Nombre: {persona.nombre}</p>
-            <p>Edad: {persona.edad}</p>
-          </div>
-        ))
-      }
+    <div>
+      <p>Hola mati como te va</p>
     </div>
   );
 }
